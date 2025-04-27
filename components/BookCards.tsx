243f41f86cc8a8c9eacab4b6feaada66beba9fca
mathanta <ethan@mattafam.com>
2025-04-27 01:08:14 -0600

    Added react book to the list of read books. Also added subtitle variable to help everything fit inside of the book card.

--- components/BookCards.tsx
+++ components/BookCards.tsx
@@ -4,18 +4,20 @@ import Image from 'next/image'
 interface BookProps {
     image: string;
     title: string;
+    subtitle: string;
     author: string;
     year: number;
 }
 
-const BookCard = ({ image, title, author, year }: BookProps) => {
+const BookCard = ({ image, title, subtitle, author, year }: BookProps) => {
     return (
         <div className={styles.card}>
             <Image src={image} alt={`Cover of ${title}`} className={styles.cover} width={100} height={150} />
             <div className={styles.info}>
                 <h3>{title}</h3>
+                {subtitle && <p className={styles.subtitle}>{subtitle}</p>}
                 <p>By {author}</p>
-                <p>Published: {year}</p>
+                <p className={styles.year}>Published: {year}</p>
             </div>
         </div>
     );
@@ -24,15 +26,24 @@ const BookCard = ({ image, title, author, year }: BookProps) => {
 export const books = [
     {
         image: "/duckett-htmlcss.jpg",
-        title: "HTML and CSS: Design and Build Websites",
+        title: "HTML and CSS:",
+        subtitle: "Design and Build Websites",
         author: "Jon Duckett",
         year: 2011
     },
     {
         image: "/duckett-javascript.jpg",
-        title: "JavaScript & jQuery: Interactive Front-End Web Development",
+        title: "JavaScript & jQuery:",
+        subtitle: "Interactive Front-End Web Development",
         author: "Jon Duckett",
         year: 2014
+    },
+    {
+        image: "/bugl-react.jpg",
+        title: "Modern Full-Stack React Projects:",
+        subtitle: "Build, maintain, and deploy modern web apps using MongoDB, Express, React, and Node.js",
+        author: "Daniel Bugl",
+        year: 2024
     }
 ];
 
